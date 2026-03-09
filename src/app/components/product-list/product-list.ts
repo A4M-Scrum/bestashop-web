966164bf37c4d6bc8462f92c806ec54562c397b6
Andrés Aragón <andresaragonguerrero@gmail.com>
2026-03-09 14:38:25 +0000

    añadido el componente de búsqueda

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,37 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ChangeDetectorRef } from '@angular/core';
 
-import {  ProductService } from '../../services/product.service';
+import { ProductService } from '../../services/product.service';
 
 import { Product } from '../../models/product.model';
 
 import { ProductCard } from '../product-card/product-card';
+import { SearchProduct } from '../search-product/search-product';
 
 @Component({
   selector: 'app-product-list',
   imports: [
     CommonModule,
     ProductCard,
+    SearchProduct,
   ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 
 export class ProductList implements OnInit {
+
   constructor(
-    private productService: ProductService, 
-    private cdRef: ChangeDetectorRef
+    private productService: ProductService
   ) { }
 
   products: Product[] = [];
+  filteredProducts: Product[] = [];
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
-      this.cdRef.detectChanges();
-
-      console.log(this.products);
+      this.filteredProducts = data;
     });
+  }
+
+  onSearch(term: string) {
+
+    const search = term.toLowerCase();
+
+    this.filteredProducts = this.products.filter(product =>
+      product.name.toLowerCase().includes(search) ||
+      product.description.toLowerCase().includes(search)
+    );
   }
 }
