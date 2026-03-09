685d9594239d642019990dd923773699ad09c82a
Andrés Aragón <andresaragonguerrero@gmail.com>
2026-03-09 19:39:48 +0000

    se añadió un componente-página para explicar el endpoint

---
+++ src/app/pages/endpoint-demo/endpoint-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endpoint-demo',
+  imports: [],
+  templateUrl: './endpoint-demo.html',
+  styleUrl: './endpoint-demo.css',
+})
+export class EndpointDemo {
+
+}
